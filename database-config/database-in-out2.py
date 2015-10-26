d933ebe7bf90a1f20c1b5c7eab46fc8b0ca8bf2f
Jan Cozijnsen <j.cozijnsen98@gmail.com>
2015-10-26 16:44:59 +0100

    Database Script Update

--- database-config/database-in-out2.py
+++ database-config/database-in-out2.py
@@ -1,1 +1,12 @@
 __author__ = 'Jan'
+import pymysql as SQL
+conn = SQL.connect(host = 'jancoz.com', port=3306, user='garage_garage', passwd='123456', db='garage_garage')
+cur = conn.cursor()
+#cur.execute("INSERT INTO Safe VALUES(1, 0)")
+def createtable():
+    #cur.execute("CREATE TABLE Auto (ID INT)")
+    cur.execute("INSERT INTO Auto VALUES (1)")
+    cur.execute("INSERT INTO Auto VALUES (2)")
+    cur.execute("INSERT INTO Auto VALUES (3)")
+createtable()
+conn.commit()
