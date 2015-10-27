cc8d6fb1eb9634e1b2ce9b1c30c91b67c9e18a4b
knesselaar <knesselaar@gmail.com>
2015-10-27 11:59:37 +0100

    Database Script Update

--- rwdapi/RDWimport.py
+++ rwdapi/RDWimport.py
@@ -1,8 +1,9 @@
 __author__ = 'kevinnesselaar'
 import requests
-url = 'https://overheid.io/api/voertuiggegevens/4-TFL-24'
+url = 'https://overheid.io/api/voertuiggegevens/4-TFL-24?fields[]=datumeersteafgiftenederland&fields=[]=kenteken'
 payload = {'ovio-api-key': '449cd9d896fd46ce22ff029805b0eb67bb6b518b7980d9173a4b0c9e659cc99b'}
 response = requests.get(url, params=payload)
+print(response.url)
 print(response.text)
 
 
