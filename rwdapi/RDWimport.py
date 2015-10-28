e83b103b52caddb933f044166f712501a457336e
knesselaar <knesselaar@gmail.com>
2015-10-28 15:43:30 +0100

    Database Script Update

--- rwdapi/RDWimport.py
+++ rwdapi/RDWimport.py
@@ -1,12 +1,51 @@
 __author__ = 'kevinnesselaar'
 import requests
-url = 'https://overheid.io/api/voertuiggegevens/4-TFL-24?fields[]=datumeersteafgiftenederland&fields=[]=kenteken'
+import csv
+kenteken = input("voer hier uw kenteken in:")
+url = 'https://overheid.io/api/voertuiggegevens/' + kenteken + '?field[]=hoofdbrandstof'
 payload = {'ovio-api-key': '449cd9d896fd46ce22ff029805b0eb67bb6b518b7980d9173a4b0c9e659cc99b'}
 response = requests.get(url, params=payload)
+
+
 print(response.url)
-print(response.text)
+resp = response.text
+print(resp)
 
 
+
+#----------------------------------------test----------------------------------------------#
+#dict(csv.reader([item], delimiter='"', quotechar="'").next()
+#         for item in csv.reader([resp], delimiter=':', quotechar="'").next())
+
+#information = (resp.split(','))
+#print(information)
+
+#dictinformation = dict(item.split(":") for item in information.split(","))
+#print(dictinformation)
+#---------------------------------------test-----------------------------------------------#
+
+
+
+
+
+
+
+#datum = int(response.text)
+#brandstof = response.text
+
+#print(datum)
+#print(brandstof)
+
+#---------------------------------------check script-----------------------------------------------#
+#if datum > 2001 and brandstof == "Diesel":
+#    print("U mag in de groene zone")
+#elif brandstof == "Benzine":
+#    print("U mag in de groene zone")
+#elif datum < 2001 or brandstof != "Diesel":
+#    print("Uw auto is niet toegestaan")
+#---------------------------------------check script-----------------------------------------------#
+
+#---------------------------------------api key-----------------------------------------------#
 #449cd9d896fd46ce22ff029805b0eb67bb6b518b7980d9173a4b0c9e659cc99b
-
-
+#[190:194][363:370]
+#---------------------------------------test-----------------------------------------------#
